636c1548102c7b2dc800e8bc05b2b048ea1305d0
Ignacio Manuel Lebrero Rial <imlrial@ARARimlrial.local>
2016-03-22 17:50:41 -0300

    matriz 2.0 :P

--- src/clases/Matriz.cpp
+++ src/clases/Matriz.cpp
@@ -17,29 +17,65 @@ private:
 
 public:
 	int filas, columnas;
-	vector< vector<int> > matriz;
+	vector< vector<float> > matriz;
 
 	Matriz() {
 		filas    = 5;
 		columnas = 5;
-		matriz = *(new vector< vector<int> > (5));
+		vector< vector<float> > fila(5, vector<float>(5));
+		matriz = asd;
 
 		for (int i = 0; i < 5; ++i) {
-			matriz.push_back( *(new vector <int> (5)) );
+			vector<float> columna(5);
+			matriz[i] = nuevo;
+			
 		}
 	}
 
 	Matriz(const int filas, const int columnas) {
 		this->filas    = filas;
 		this->columnas = columnas;
-		matriz = *(new vector< vector<int> > (filas));
+		matriz = *(new vector< vector<float> > (filas));
 
 		for (int i = 0; i < filas; ++i) {
-			matriz.push_back( *(new vector <int> (columnas)) );
+			matriz.push_back( *(new vector <float> (columnas)) );
 		}
 	}
 
-	Matriz& operator+ (const Matriz& m) {
+	/*******************************
+	 *          Operadores         *
+	 *******************************/ 
+
+	Matriz& operator * (const int i) {
+		for (int f = 0; f < filas; ++f) {
+			for (int c = 0; c < columnas; ++c) {
+				matriz [f][c] *= i;
+			}
+		}
+		
+		return *this;
+	}
+
+	Matriz& operator * (const Matriz& m) {
+		Matriz* nueva = NULL;
+
+		if (this->columnas == m.filas) {
+			Matriz* nueva = new Matriz(m.filas, m.filas);
+
+			for (int f = 0; f < this->filas; ++f) {
+				for (int c = 0; c < this->columnas; ++c) {
+					for (int k = 0; k < this->columnas; ++k) {
+						nueva->matriz[f][c] += this->matriz[f][k] + m.matriz[k][c];
+					}
+				}
+			}
+
+		}
+
+		return *nueva;
+	}
+
+	Matriz& operator + (const Matriz& m) {
 		if (m.filas == this->filas && m.columnas == this->columnas) {
 		
 			for (int f = 0; f < filas; ++f) {
@@ -53,7 +89,7 @@ public:
 		return *this;
 	}
 
-	Matriz& operator- (const Matriz& m) {
+	Matriz& operator - (const Matriz& m) {
 		if (m.filas == this->filas && m.columnas == this->columnas) {
 		
 			for (int f = 0; f < filas; ++f) {
@@ -67,40 +103,7 @@ public:
 		return *this;
 	}
 
-	Matriz& operator* (const Matriz& m) { 
-		Matriz* nueva = NULL;
-
-		if (this->columnas == m.filas) {
-			Matriz* nueva = new Matriz(m.filas, m.filas);
-
-			for (int f = 0; f < this->filas; ++f) {
-				for (int c = 0; c < this->columnas; ++c) {
-					for (int k = 0; k < this->columnas; ++k) {
-						nueva[f][c] += this->matriz[f][k] + m.matriz[k][c];
-					}
-				}
-			}
-
-		}
-
-		return *nueva;
-	}
-
-	void set(const int i, const int j, int valor) {
-		this->matriz[i][j] = valor;
-	}
-
-	int get(const int i, const int j) {
-		return this->matriz[i][j];
-	}
-
-	Matriz& operator* (const int i) {
-		for (int f = 0; f < filas; ++f) {
-			for (int c = 0; c < columnas; ++c) {
-				matriz [f][c] *= i;
-			}
-		}
-		
-		return *this;
-	}
+	vector<float>& operator [] (const int fila) {
+        return this->matriz[fila];
+    }
 };
